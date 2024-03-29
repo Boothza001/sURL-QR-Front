5496999207f1647b1ae58eac0027627eb7ac683a
Booth <ugrit.booth@gmail.com>
2024-03-30 04:24:27 +0700

    Update CardMain.jsx

--- src/components/CardMain.jsx
+++ src/components/CardMain.jsx
@@ -4,8 +4,8 @@ import { DeleteIcon } from "@chakra-ui/icons";
 import QRCode from "qrcode.react";
 
 function CardMain({ url, shortUrl, count, onDelete }) {
-  // const svaddr = "https://surl-qr-back-2.onrender.com";
-  const svaddr = "http://localhost:3000";
+  const svaddr = "https://surl-qr-back-2.onrender.com";
+  // const svaddr = "http://localhost:3000";
 
   // const [clicked, setClicked] = useState(false);
 
