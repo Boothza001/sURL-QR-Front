b1c5d3b7abfe10ad7278134844d1e9ee21a719fe
Booth <ugrit.booth@gmail.com>
2024-03-30 13:27:52 +0700

    Revert "Update CardMain.jsx"
    This reverts commit 42d26916f52bf1ca744308c78f5f273fc3af6e78.

--- src/components/CardMain.jsx
+++ src/components/CardMain.jsx
@@ -20,7 +20,7 @@ function CardMain({ url, shortUrl, count, onDelete }) {
 
   const handleShortUrlClick = async () => {
     try {
-      const response = await fetch(`${svaddr}/${shortUrl}`);
+      const response = await fetch(`${svaddr}/api/redirect/${shortUrl}`);
       window.open(url, "_blank");
       if (response.ok) {
         const data = await response.json();
